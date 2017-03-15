7ceec3525997fce6c32392d18c0433b16a87b0d6
Markus Heiser <markus.heiser@darmarit.de>
2017-03-15 23:33:31 +0100

    Radicale: WSGI CalDAV & CardDAV Server
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

---
+++ scripts/radicale.sh
@@ -0,0 +1,114 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8; mode: sh -*-
+# ----------------------------------------------------------------------------
+# Purpose:     gitlab radicale with apache
+# ----------------------------------------------------------------------------
+
+source $(dirname ${BASH_SOURCE[0]})/setup.sh
+#setupInfo
+sudoOrExit
+
+# ----------------------------------------------------------------------------
+# Config
+# ----------------------------------------------------------------------------
+
+WSGI_APPS="${WWW_FOLDER}/pyApps"
+PYENV=pyenv
+
+RADICALE_GIT_URL="https://github.com/Kozea/Radicale.git"
+RADICALE_DATA_FOLDER="$WSGI_APPS/Radicale.data"
+RADICALE_REPO_FOLDER="$WSGI_APPS/Radicale"
+
+DATA_BACKUP=(
+    "$RADICALE_DATA_FOLDER"
+)
+
+# ----------------------------------------------------------------------------
+main(){
+    rstHeading "Radicale" part
+# ----------------------------------------------------------------------------
+
+    case $1 in
+	install)
+            install_radicale
+	    ;;
+	deinstall)
+            deinstall_radicale
+	    ;;
+            ;;
+	*)
+            echo
+	    echo "usage $0 [(de)install]"
+            echo
+            ;;
+    esac
+}
+# ----------------------------------------------------------------------------
+install_radicale(){
+    rstHeading "Installation Radicale"
+# ----------------------------------------------------------------------------
+
+    README_GITLAB
+    if ! askYn "Soll Radicale installiert werden?"; then
+        return 42
+    fi
+
+    if ! aptPackageInstalled apache2; then
+
+        rstBlock "Apache is noch nicht installiert, die Installation sollte mit
+dem Skript 'apache_setup.sh' durchgeführt werden."
+        return 42
+    fi
+
+    cloneGitRepository "${RADICALE_GIT_URL}" "${RADICALE_REPO_FOLDER}"
+    TEMPLATES_InstallOrMerge /var/www/pyApps/Radicale/radicale.wsgi root root 644
+    source ${WSGI_APPS}/${PYENV}/bin/activate
+    pip install vobject
+
+    APACHE_install_site radicale
+
+    rstBlock "
+
+  https://$HOSTNAME/radicale/<username>
+"
+
+}
+
+
+# ----------------------------------------------------------------------------
+deinstall_gitlab(){
+    rstHeading "Radicale"
+# ----------------------------------------------------------------------------
+
+    rstBlock "${BRed}ACHTUNG:${_color_Off}
+
+    Folgende Aktion löscht die Radicale samt Konfiguration!"
+
+    if ! askNy "Wollen sie WIRKLICH die Konfiguration löschen?"; then
+        return 42
+    fi
+
+    rstHeading "Apache Site *disable*" section
+    echo
+    TEE_stderr <<EOF | bash | prefix_stdout
+a2dissite radicale
+systemctl force-reload apache2
+EOF
+
+    rstHeading "Aufräumen" section
+
+    echo -e "
+Folgende Dateien bzw. Ordner wurden nicht gelöscht:
+
+* Anwendungsdaten: ${BYellow}${RADICALE_DATA_FOLDER}${_color_Off}
+* Reposetorie:     ${BYellow}${RADICALE_REPO_FOLDER}${_color_Off}
+
+Diese müssen ggf. gesichert und anschließend gelöscht werden."
+
+    waitKEY
+
+}
+
+# ----------------------------------------------------------------------------
+main "$@"
+# ----------------------------------------------------------------------------
---
+++ templates/var/www/pyApps/Radicale/radicale.wsgi
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+#
+# This file is part of Radicale Server - Calendar Server
+# Copyright © 2011-2016 Guillaume Ayoub
+#
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Radicale.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Radicale WSGI file (mod_wsgi and uWSGI compliant).
+
+"""
+import site
+site.addsitedir('/var/www/pyApps/Radicale')
+
+import os
+from radicale import Application, config, log
+
+
+configuration = config.load(["/var/www/pyApps/Radicale.data/config",])
+
+logger = log.start()
+application = Application(configuration, logger)
