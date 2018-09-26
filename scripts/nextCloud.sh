a13b1cbc6fd572c185a98d87fa263f7c23e74772
Markus Heiser <markus.heiser@darmarit.de>
2018-09-26 13:24:48 +0200

    nextCloud: inital (WIP) / Hinweise aus ArchLinux eingearbeitet

--- scripts/nextCloud.sh
+++ scripts/nextCloud.sh
@@ -37,10 +37,14 @@ NEXTCLOUD_PACKAGES="\
 
 NEXTCLOUD_APACHE_SITE="nextCloud"
 NEXTCLOUD_URL_ALIAS="cloud"
-NEXTCLOUD_ROOT="${PHP_APPS}/nextCloud"
+NEXTCLOUD_ROOT="${PHP_APPS}/nextcloud"
 
 NEXTCLOUD_ALLOW="Allow from all"
 #NEXTCLOUD_ALLOW="Allow from fd00::/8 192.168.0.0/16 fe80::/10 127.0.0.0/8 ::1"
+
+NEXTCLOUD_DOWNLOAD_URL=https://download.nextcloud.com/server/releases/
+# leave this empty to get *latest*
+#NEXTCLOUD_VERSION=14.0.1
 
 # maraiDB setup
 # -------------
@@ -71,39 +75,54 @@ DATA_BACKUP=(
 )
 
 
+
 # ----------------------------------------------------------------------------
 info(){
 # ----------------------------------------------------------------------------
 
     rstBlock "Die Installation von nextCloud erfolgt hinter einem WEB-Server.
-Die nextCloud Anwendung selbst wird danach online eingerichtet.
+Die nextCloud Anwendung selbst wird danach online eingerichtet."
 
-Bei der hier vorgeschlagenen Installation wird die nextCloud hinter einem Apache
-WEB Server installiert.  Sofern der erforderliche WEB-Server und die Komponenten
-nicht bereits installiert sind, werden diese installiert.  Die Installation des
-Apache Servers nutzt die Methoden und Skripte, die in den handsOn bereitstehen,
-mehr dazu kann man unter [1] nachlesen.
+    rstHeading "WEB-Server Apache" section
 
-Neben dem WEB-Server benötigt nextCloud noch eine Datenbank. Es wird der von
-nextCloud empfohlene DB-Server mariaDB [2] installiert [3]. mariaDB ist der
-(echte) Open-Source Nachfolger von MySQL. Eine MySQL Abspaltung aus dem Jahre
-2009, die inzwischen stabiler und performanter ist als MySQL, welches in den
-Fuchteln von Oracle dem Marketing geschuldet kaputt gefrickelt wird.
+    rstBlock "Bei der hier vorgeschlagenen Installation wird die nextCloud
+hinter einem Apache WEB Server installiert.  Sofern der erforderliche WEB-Server
+und die Komponenten nicht bereits installiert sind, werden diese installiert.
+Die Installation des Apache Servers nutzt die Methoden und Skripte, die in den
+handsOn bereitstehen, mehr dazu kann man unter [1] nachlesen."
 
-Entgegen den z.T. noch veralteten Dokumentationen wird bei der DB Installation
-kein Passwort mehr vergeben. Die Standard Installation bei debian/Ubuntu
-verwendet zur Authentifizierung das Plugin 'Unix Socket' [4]. Die root-Anmeldung
-an der DB erfolgt als root Benutzer des localhost ohne weitere Passwort-Abfrage.
-So kann man sich dann mittels sudo an der DB als root (User der DB) anmelden, ohne
-dass man dafür ein Passwort eingeben muss::
+    rstHeading "DB-Server mariaDB" section
+
+    rstBlock "Neben dem WEB-Server benötigt nextCloud noch eine Datenbank.  Es
+wird der von nextCloud empfohlene DB-Server mariaDB [2] installiert [3]. mariaDB
+ist der (echte) Open-Source Nachfolger von MySQL. Eine MySQL Abspaltung aus dem
+Jahre 2009, die inzwischen stabiler und performanter ist als MySQL, welches in
+den Fuchteln von Oracle dem Marketing geschuldet kaputt gefrickelt wird.
+
+Entgegen den z.T. noch veralteten (Stand 09/2018) Dokumentationen [5] wird bei
+der DB Installation kein Passwort mehr vergeben. Die Standard Installation bei
+debian/Ubuntu verwendet zur Authentifizierung das Plugin 'Unix Socket' [4]. Die
+root-Anmeldung an der DB erfolgt als root Benutzer des localhost ohne weitere
+Passwort-Abfrage.  So kann man sich dann mittels sudo an der DB als root (User
+der DB) anmelden, ohne dass man dafür ein Passwort eingeben muss::
 
   sudo mariadb
 "
-    echo "\
+
+    rstHeading "DB-Server mariaDB" section
+
+    rstBlock "Die exemplarische Installation für Ubuntu 16.04 LTS, die man bei
+nextCloud findet [5] ist leider nicht so gut.  Eine gute Anleitung zur
+Installation gibt es bei ArchLinux [6], man muss dabei nur wissen, dass die
+Pfade & Pakete auf Debian anders sind als auf ArchLinux."
+
+    echo "
 [1] https://return42.github.io/handsOn/apache_setup/index.html
 [2] https://mariadb.com
 [3] https://mariadb.com/kb/en/library/installing-mariadb-deb-files
 [4] https://mariadb.com/kb/en/library/authentication-plugin-unix-socket/
+[5] https://docs.nextcloud.com/server/14/admin_manual/installation/source_installation.html
+[6] https://wiki.archlinux.org/index.php/Nextcloud
 "
 }
 
@@ -127,7 +146,6 @@ EOF
 
 # ----------------------------------------------------------------------------
 main(){
-    rstHeading "nextCloud" part
 # ----------------------------------------------------------------------------
 
     case $1 in
@@ -170,11 +188,13 @@ main(){
 
 # ----------------------------------------------------------------------------
 install_nextCloud_server(){
-    rstHeading "Installation nextCloud"
+    rstHeading "Installation nextCloud" part
 # ----------------------------------------------------------------------------
 
+    info
+    waitKEY
 
-    install_mariaDB
+    rstHeading "Installation der Voraussetzungen"
 
     if ! aptPackageInstalled apache2 ; then
 	rstBlock "Apache ist noch nicht installiert. Für die nextCloud
@@ -196,15 +216,68 @@ noch nicht installiert. Die Basis-Installation für PHP wird nun vorgenommen ...
 nun alle weiteren, von nextCloud benötigten (APT) Pakete installiert."
 
     aptInstallPackages ${NEXTCLOUD_PACKAGES}
-    waitKEY
 
+    install_mariaDB
+    download_install_nextcloud
+
+    rstHeading "Einrichten der Apache-Site für nextCloud"
     APACHE_install_site --eval ${NEXTCLOUD_APACHE_SITE}
     echo
     echo "Zur nextCloud Seite --> https://$(uname -n)/${NEXTCLOUD_URL_ALIAS}"
     waitKEY
-    # FIXME: .... nun die nextCloud Installation
 }
 
+# ----------------------------------------------------------------------------
+download_install_nextcloud(){
+    rstHeading "download & install nextCloud $(nextcloud_version)"
+# ----------------------------------------------------------------------------
+    echo
+    NEXTCLOUD_TAR="nextcloud-$(nextcloud_version).tar.bz2"
+
+    cacheDownload "${NEXTCLOUD_DOWNLOAD_URL}/${NEXTCLOUD_TAR}" "${NEXTCLOUD_TAR}" askYn
+
+    rm -rf "${CACHE}/nextCloud"
+    mkdir -p "${CACHE}/nextCloud"
+    tar -C "${CACHE}/nextCloud" -xjf "${CACHE}/${NEXTCLOUD_TAR}" | prefix_stdout
+    if [[ -d "${NEXTCLOUD_ROOT}" ]]; then
+	echo "Alte Installation wird verschoben: ${NEXTCLOUD_ROOT}_$(timestamp)"
+	mv "${NEXTCLOUD_ROOT}"  "${NEXTCLOUD_ROOT}_$(timestamp)"
+    fi
+    waitKEY
+    installFolder "${CACHE}/nextCloud/nextcloud" "${NEXTCLOUD_ROOT}/.." root ${WWW_USER}
+
+    rstHeading "Anlegen benötigter Ordner" section
+    echo
+    TEE_stderr <<EOF | bash | prefix_stdout
+    mkdir -p "${NEXTCLOUD_ROOT}/data"
+    mkdir -p "${NEXTCLOUD_ROOT}/assets"
+EOF
+    waitKEY
+
+    rstHeading "Berechtigungen setzen" section
+    echo
+    TEE_stderr <<EOF | bash | prefix_stdout
+find ${NEXTCLOUD_ROOT}/ -type f -print0 | xargs -0 chmod 0640
+find ${NEXTCLOUD_ROOT}/ -type d -print0 | xargs -0 chmod 0750
+
+chown -R root:${WWW_USER} ${NEXTCLOUD_ROOT}/
+chown -R ${WWW_USER}:${WWW_USER} ${NEXTCLOUD_ROOT}/apps/
+chown -R ${WWW_USER}:${WWW_USER} ${NEXTCLOUD_ROOT}/assets/
+chown -R ${WWW_USER}:${WWW_USER} ${NEXTCLOUD_ROOT}/config/
+chown -R ${WWW_USER}:${WWW_USER} ${NEXTCLOUD_ROOT}/data/
+chown -R ${WWW_USER}:${WWW_USER} ${NEXTCLOUD_ROOT}/themes/
+chown -R ${WWW_USER}:${WWW_USER} ${NEXTCLOUD_ROOT}/updater/
+
+chmod +x ${NEXTCLOUD_ROOT}/occ
+
+[ -f ${NEXTCLOUD_ROOT}/.htaccess ] && chmod 0644 ${NEXTCLOUD_ROOT}/.htaccess
+[ -f ${NEXTCLOUD_ROOT}/.htaccess ] && chown root:${WWW_USER} ${NEXTCLOUD_ROOT}/.htaccess
+[ -f ${NEXTCLOUD_ROOT}/data/.htaccess ] && chmod 0644 ${NEXTCLOUD_ROOT}/data/.htaccess
+[ -f ${NEXTCLOUD_ROOT}/data/.htaccess ] && chown root:${WWW_USER} ${NEXTCLOUD_ROOT}/data/.htaccess
+EOF
+    waitKEY
+
+}
 
 # ----------------------------------------------------------------------------
 activate_server() {
@@ -234,7 +307,7 @@ EOF
 
 # ----------------------------------------------------------------------------
 remove_nextCloud_server() {
-    rstHeading "De-Installation nextCloud"
+    rstHeading "De-Installation nextCloud" part
 # ----------------------------------------------------------------------------
 
     if ! askYn "Soll nextCloud deinstalliert werden?"; then
@@ -272,8 +345,6 @@ entfernt werden ..."
 install_mariaDB(){
     rstHeading "Installation der mariaDB"
 # ----------------------------------------------------------------------------
-
-    rstBlock "Im Folgendem wird ein mariaDB Server eingerichtet."
 
     aptInstallPackages ${MARIADB_PACKAGES}
     systemctl status mariadb.service
@@ -318,6 +389,17 @@ systemctl disable mariadb.service
 EOF
     waitKEY 10
 }
+
+nextcloud_version(){
+    if [[ -z $NEXTCLOUD_VERSION ]]; then
+	NEXTCLOUD_VERSION=$(curl "$NEXTCLOUD_DOWNLOAD_URL" 2>/dev/null \
+				| sed -n 's/.*href="nextcloud-\([0-9]*.[0-9]*.[0-9]*\).*/\1/p' \
+				| sort -V \
+				| tail -1 )
+    fi
+    echo $NEXTCLOUD_VERSION
+}
+
 
 # ----------------------------------------------------------------------------
 main "$@"
