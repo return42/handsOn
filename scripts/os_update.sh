bab4b228f1961b08250a235e4f1d0a89c0c03016
Markus Heiser <markus.heiser@darmarit.de>
2019-04-08 11:21:31 +0200

    os_update: Fehlerbehandlung falls Systemupdate fehlschlägt
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- scripts/os_update.sh
+++ scripts/os_update.sh
@@ -19,7 +19,22 @@ case "$(lsb_release -si)" in
     Ubuntu|Debian)
 
 	apt-get update -y
-	apt-get dist-upgrade -y
+        echo "xxx"
+	if ! apt-get dist-upgrade -y;  then
+
+            rstBlock "${BRed}Es gab ein Problem beim Update des Systems${_color_Off}
+
+Bitte lesen Sie obige Fehlermeldungen und entscheiden ob eine Fehlerbehandlung
+erforderlich ist.  Steht dort etwas wie 'konnte Sperre nicht bekommen', dann
+läuft parallel ein anderes Programm, dass Software installiert (oder ein
+Systemupate macht).
+
+In diesem Fall brechen Sie dieses handsOn Update erst mal mit STRG-C
+ab.  Das andere Programm muss man beenden oder zu ende laufen
+lassen und dann nochmal dieses handsOn Update NEU starten."
+            waitKEY
+
+        fi
 	apt-get autoclean -y
 	apt-get autoremove -y
 	FORCE_TIMEOUT=0 "${SCRIPT_FOLDER}/vbox.sh" update
