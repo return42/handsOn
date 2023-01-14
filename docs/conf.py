cad34597f1594689fa7a6d81ae505ec892639f6c
Markus Heiser <markus.heiser@darmarit.de>
2023-01-14 16:02:09 +0100

    [boilerplate] Sphinx build der Doku aktualisiert
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- docs/conf.py
+++ docs/conf.py
@@ -54,14 +54,14 @@ intersphinx_mapping['python']  = ('https://docs.python.org/', None)
 
 extlinks = {}
 # usage:    :man:`make`
-extlinks['man']       = ('http://manpages.ubuntu.com/cgi-bin/search.py?q=%s', '')
-#extlinks['man']      = ('http://manpages.ubuntu.com/manpages/' + language + '/%s.html', '')
-extlinks['deb']       = ('http://packages.ubuntu.com/%s', '')
-#extlinks['rfc']      = ('https://tools.ietf.org/html/rfc%s', 'RFC ')
-extlinks['launchpad'] = ('https://launchpad.net/%s/trunk', 'launchpad')
-extlinks['origin']    = ('https://github.com/return42/handsOn/blob/master/%s', 'git')
-extlinks['commit']    = ('https://github.com/return42/handsOn/commit/%s', '#')
-extlinks['apache_mod']  = ('https://httpd.apache.org/docs/current/mod/%s.html', 'Apache ')
+extlinks['man']       = ('http://manpages.ubuntu.com/cgi-bin/search.py?q=%s', '%s')
+#extlinks['man']      = ('http://manpages.ubuntu.com/manpages/' + language + '/%s.html', '%s')
+extlinks['deb']       = ('http://packages.ubuntu.com/%s', '%s')
+#extlinks['rfc']      = ('https://tools.ietf.org/html/rfc%s', 'RFC %s')
+extlinks['launchpad'] = ('https://launchpad.net/%s/trunk', '%s@launchpad')
+extlinks['origin']    = ('https://github.com/return42/handsOn/blob/master/%s', 'git://%s')
+extlinks['commit']    = ('https://github.com/return42/handsOn/commit/%s', '#%s')
+extlinks['apache_mod']  = ('https://httpd.apache.org/docs/current/mod/%s.html', 'Apache %s')
 
 
 html_search_language = 'de'
